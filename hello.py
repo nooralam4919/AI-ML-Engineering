964ac1cefebeaf5c5c9ef17228d12e1c0b65b937
Noor Alam <alamn5063@gmail.com>
2026-09-21 02:52:00 +0530

    env file change

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-import pandas as pd
+api_key = os.environ.get("API_KEY")
+database_key = os.environ.get("DATABASE_KEY")
+
+print(api_key)
+print(database_key)
